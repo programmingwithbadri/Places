2d207ef7c91e1d404af0d2369c4575e3bdadcbf9
Badri Narayanan <programmingwithbadri@outlook.com>
2021-05-13 20:52:12 +0530

    Get the user location

---
+++ components/LocationPicker.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  Button,
+  Text,
+  ActivityIndicator,
+  Alert,
+  StyleSheet
+} from 'react-native';
+import * as Location from 'expo-location';
+
+import Colors from '../constants/Colors';
+
+const LocationPicker = props => {
+  const [isFetching, setIsFetching] = useState(false);
+  const [pickedLocation, setPickedLocation] = useState();
+
+  const mapPickedLocation = props.navigation.getParam('pickedLocation');
+
+  const { onLocationPicked } = props;
+
+  useEffect(() => {
+    if (mapPickedLocation) {
+      setPickedLocation(mapPickedLocation);
+      onLocationPicked(mapPickedLocation);
+    }
+  }, [mapPickedLocation, onLocationPicked]);
+
+  const verifyPermissions = async () => {
+    const result = await Location.requestForegroundPermissionsAsync();
+    if (result.status !== 'granted') {
+      Alert.alert(
+        'Insufficient permissions!',
+        'You need to grant location permissions to use this app.',
+        [{ text: 'Okay' }]
+      );
+      return false;
+    }
+    return true;
+  };
+
+  const getLocationHandler = async () => {
+    const hasPermission = await verifyPermissions();
+    console.log(hasPermission)
+    if (!hasPermission) {
+      return;
+    }
+
+    try {
+      setIsFetching(true);
+      const location = await Location.getCurrentPositionAsync({
+        timeout: 5000
+      });
+
+      console.log(location)
+      setPickedLocation({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude
+      });
+      props.onLocationPicked({
+        lat: location.coords.latitude,
+        lng: location.coords.longitude
+      });
+    } catch (err) {
+      Alert.alert(
+        'Could not fetch location!',
+        'Please try again later or pick a location on the map.',
+        [{ text: 'Okay' }]
+      );
+    }
+    setIsFetching(false);
+  };
+
+  return (
+    <View style={styles.locationPicker}>
+      <View style={styles.actions}>
+        <Button
+          title="Get User Location"
+          color={Colors.primary}
+          onPress={getLocationHandler}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  locationPicker: {
+    marginBottom: 15
+  },
+  mapPreview: {
+    marginBottom: 10,
+    width: '100%',
+    height: 150,
+    borderColor: '#ccc',
+    borderWidth: 1
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%'
+  }
+});
+
+export default LocationPicker;
--- screens/NewPlaceScreen.js
+++ screens/NewPlaceScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import {
   ScrollView,
@@ -12,11 +12,13 @@ import {
 import Colors from '../constants/Colors';
 import * as placesActions from '../store/places-actions';
 import ImagePicker from '../components/ImagePicker';
+import LocationPicker from '../components/LocationPicker';
 
 const NewPlaceScreen = props => {
   const dispatch = useDispatch();
   const [titleValue, setTitleValue] = useState('');
   const [selectedImage, setSelectedImage] = useState();
+  const [selectedLocation, setSelectedLocation] = useState();
 
   const titleChangeHandler = text => {
     setTitleValue(text);
@@ -25,6 +27,10 @@ const NewPlaceScreen = props => {
   const imageTakenHandler = imagePath => {
     setSelectedImage(imagePath);
   };
+
+  const locationPickedHandler = useCallback(location => {
+    setSelectedLocation(location);
+  }, []);
 
   const savePlaceHandler = () => {
     dispatch(placesActions.addPlace(titleValue, selectedImage));
@@ -41,6 +47,10 @@ const NewPlaceScreen = props => {
           value={titleValue}
         />
         <ImagePicker onImageTaken={imageTakenHandler} />
+        <LocationPicker
+          navigation={props.navigation}
+          onLocationPicked={locationPickedHandler}
+        />
         <Button
           title="Save Place"
           color={Colors.primary}
